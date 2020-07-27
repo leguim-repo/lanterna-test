90208aace2828471326ec12ce8e0d5130745a89f
mike <60325499+leguim-repo@users.noreply.github.com>
2020-07-27 09:10:58 +0200

    Primeras pruebas de Lanterna

---
+++ src/main/java/com/mike/lanterna/test/App.java
@@ -0,0 +1,22 @@
+package com.mike.lanterna.test;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException {
+
+
+    }
+}
---
+++ src/test/java/com/mike/lanterna/test/AppTest.java
@@ -0,0 +1,20 @@
+package com.mike.lanterna.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
